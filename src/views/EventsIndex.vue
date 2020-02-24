6916549b76355d247b417d8ae3048a82dbc062cb
“dziekan30” <dziekan30@gmail.com>
2020-02-24 15:42:34 -0600

    new with Aut

---
+++ src/views/EventsIndex.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="events-index">
+    <h1>All Events</h1>
+    <div v-for="event in events">
+      <router-link v-bind:to="'/events' + event.id">
+        <h2> {{ event.name }}</h2>
+      </router-link>
+      <h2>{{event.description}}</h2>
+      <br>
+    </div>
+  </div>
+</template>
+
+<style>
+</style>
+
+<script>
+  var axios = require("axios");
+
+export default {
+  data: function() {
+    return {
+      events: []
+    };
+  },
+  created: function() {
+    axios
+      .get("/api/events")
+      .then(response => {
+        this.events = response.data;
+      });
+  },
+  methods: {}
+};
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,31 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1></h1>
   </div>
 </template>
 
-<script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+<style>
+</style>
 
+<script>
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
-}
+  data: function() {
+    return {
+      message: "Welcome to Vue.js!"
+    };
+  },
+  created: function() {},
+  methods: {}
+};
 </script>
+
+<!-- 
+
+
+user_id: params[:user_id],
+                        name: params[:name],
+                        category: params[:category],
+                        location: params[:location],
+                        description: params[:description],
+                        start_time: params[:start_time],
+                        end_time: params[:end_time], -->
